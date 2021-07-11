538965c1d9ee2b011adb0c2cc10a34e718faea5f
Tomast1337 <vycasnicolas@gmail.com>
2021-07-10 23:15:55 -0300

    Acerações em Server utilizando nova classe SeverThreads

---
+++ src/io/github/tomast13337/Server/Server.java
@@ -0,0 +1,49 @@
+package io.github.tomast13337.Server;
+
+import io.github.tomast13337.Main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server implements Runnable {
+    private ServerSocket serverSocket;
+    private boolean running;
+    private ArrayList clientsThreads;
+
+    public Server(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        Main.logger.info("Servidor iniciado na porta " + String.valueOf(port));
+        this.clientsThreads = new ArrayList<ServerThread>();
+        while (this.running) {
+
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (this.running) {
+                Main.logger.info("Servidor esperando nova conexão com clientes...");
+                Socket newClientSocket = serverSocket.accept();
+                Main.logger.info("Nova conexão feita");
+                ServerThread newThread = new ServerThread(newClientSocket);
+                newThread.start();
+                clientsThreads.add(newThread);
+            }
+        } catch (IOException e) {
+            Main.logger.info(e.getMessage());
+        }
+    }
+
+    public void SendToClients() {
+
+    }
+
+    public void kill() throws IOException {
+        running = false;
+        serverSocket.close();
+        Main.logger.info("Servidor fechado");
+    }
+}
