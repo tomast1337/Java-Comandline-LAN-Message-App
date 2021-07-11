625562398f96f466db2860e1e2d5886ba5031450
Tomast1337 <vycasnicolas@gmail.com>
2021-07-10 23:13:16 -0300

    Criado classe ServerThread

---
+++ src/io/github/tomast13337/Server/ServerThread.java
@@ -0,0 +1,43 @@
+package io.github.tomast13337.Server;
+
+import io.github.tomast13337.Cliente.Client;
+import io.github.tomast13337.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+
+    PrintWriter out;
+    BufferedReader in;
+    Socket connection;
+    String name;
+
+    public ServerThread(Socket connection) throws IOException {
+        this.connection = connection;
+        this.in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        this.out = new PrintWriter(connection.getOutputStream(), true);
+    }
+
+    @Override
+    public void run() {
+        String inputLine, outputLine;
+        try {
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = inputLine;
+                if (outputLine.equalsIgnoreCase("/sair"))
+                    Main.logger.info("Cliente desconectado.");
+                break;
+            }
+        } catch (IOException e) {
+            Main.logger.info(e.getMessage());
+        }
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+}
