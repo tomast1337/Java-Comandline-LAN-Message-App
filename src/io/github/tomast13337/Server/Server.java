09d4e41d09481f7f81ae0f0f4bc25a696f314b48
Tomast1337 <vycasnicolas@gmail.com>
2021-07-11 04:04:57 -0300

    Alterações em Server

--- src/io/github/tomast13337/Server/Server.java
+++ src/io/github/tomast13337/Server/Server.java
@@ -7,38 +7,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Server extends Thread {
-    private ServerSocket serverSocket;
+public class Server implements Runnable {
+    private final ServerSocket serverSocket;
+    private ArrayList<ServerThread> clients = new ArrayList<ServerThread>();
     private boolean running;
-    private ArrayList clientsThreads;
+
 
     public Server(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
+        running = false;
         Main.logger.info("Servidor iniciado na porta " + String.valueOf(port));
-        this.clientsThreads = new ArrayList<ServerThread>();
-        while (this.running) {
-
-        }
     }
 
     @Override
     public void run() {
+        running = true;
         try {
             while (this.running) {
-                Main.logger.info("Servidor esperando nova conexão com clientes...");
-                Socket newClientSocket = serverSocket.accept();
+                Main.logger.info("Servidor pronto para fazer uma nova conexão com clientes...");
+                Socket newConnection = serverSocket.accept();
+                ServerThread newClient = new ServerThread(newConnection, clients);
+                newClient.start();
+                clients.add(newClient);
                 Main.logger.info("Nova conexão feita");
-                ServerThread newThread = new ServerThread(newClientSocket);
-                newThread.start();
-                clientsThreads.add(newThread);
             }
         } catch (IOException e) {
             Main.logger.info(e.getMessage());
         }
-    }
-
-    public void SendToClients() {
-
     }
 
     public void kill() throws IOException {
