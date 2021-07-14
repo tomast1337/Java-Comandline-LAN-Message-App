d6dd156f44644a31a9264169d644a06cf5112415
Tomast1337 <vycasnicolas@gmail.com>
2021-07-14 16:50:22 -0300

    Adicionado verificações para o nome do cliente Removido campo connection

--- src/io/github/tomast13337/Server/ServerThread.java
+++ src/io/github/tomast13337/Server/ServerThread.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -15,22 +16,33 @@ public class ServerThread extends Thread {
 
     private final PrintWriter out;
     private final BufferedReader in;
-    private final Socket connection;
+    private Server server;
     private String nome = "";
 
-    public ServerThread(Socket connection) throws IOException {
-        this.connection = connection;
+    public ServerThread(Socket connection, Server server) throws IOException {
         this.in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         this.out = new PrintWriter(connection.getOutputStream(), true);
+        this.server = server;
     }
 
     @Override
     public void run() {
         try {
             nome = in.readLine();
+            if (nome.isEmpty()) {
+                out.println("O nome não pode ser vazio");
+                return;
+            }
+
+            if (server.ExisteNome(nome)) {
+                out.println("O nome " + nome + " já esta sendo utilizado");
+                return;
+            }
+
             Main.logger.info(nome + " entrou no chat");
             while (true) {
-                if (!in.ready()) continue;
+                if (!in.ready())
+                    continue;
                 String inputLine = in.readLine();
                 if (inputLine.equalsIgnoreCase("/sair"))
                     break;
